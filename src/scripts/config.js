cebee861a161cc186e062377b6d876f220c072a5
lxp <lixueping906@gmail.com>
2021-04-22 15:08:03 +0800

    up

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -4,15 +4,11 @@ let environmentVar = ''
 let sparkUrlVar = '' // 接口地址
 let basePathVar = '' // history模式下的路径后缀
 
-// environmentVar = '开发'
-// environmentVar = '测试'
-environmentVar = '正式'
+environmentVar = '开发'
+// environmentVar = '正式'
 
 if(environmentVar === '开发') {
-    sparkUrlVar = 'http://59.110.6.209/v1'
-    basePathVar = '/'
-} else if(environmentVar === '测试') {
-    sparkUrlVar = ''
+    sparkUrlVar = 'https://api.esportlivescore.cn/els'
     basePathVar = '/'
 } else if(environmentVar === '正式') {
     sparkUrlVar = 'http://47.75.63.34/els'
