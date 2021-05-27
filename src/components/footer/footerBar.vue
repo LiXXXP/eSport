50a91fc0febafac5af3491059a70d12a833102ac
lxp <lixueping906@gmail.com>
2021-05-27 18:52:39 +0800

    up

--- src/components/footer/footerBar.vue
+++ src/components/footer/footerBar.vue
@@ -18,7 +18,10 @@
             <img src="../../assets/imgs/index/footer.png">
             <div>
                 <p>2019© All Rights Reserved.esportlivescore.com</p>
-                <p>经营许可证：京ICP备11025276号-3 <img src="../../assets/h.png" class="h" />京公网安备 11010502044856</p>
+                <p>
+                    经营许可证：<a href="https://beian.miit.gov.cn/#/Integrated/index" target="_blank">京ICP备11025276号-3</a>
+                    <img src="../../assets/h.png" class="h" />京公网安备 <a href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=11010502044856" target="_blank">11010502044856</a>
+                </p>
                 <p>声明：本网为非赢利站点，数据仅供电竞爱好者浏览使用,禁止非法滥用。</p>
                 <p>网站登载广告均为客户表达方式和本网无关,内容如有违反国家法律规定的,网站有权随时予以删除。</p>
             </div>
@@ -146,6 +149,9 @@
                margin: 0 5px;
                vertical-align: middle;
                display: inline-block;
+            }
+            a {
+              color: #337ab7;
             }
         }
     }
